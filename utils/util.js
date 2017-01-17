a89b08fcf7229d5075b2beec79c829e541d06a39
tkyzz <zhuyicui@163.com>
2017-01-17 17:18:58 +0800

    fix

--- utils/util.js
+++ utils/util.js
@@ -85,6 +85,45 @@ function formatNumber(n) {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
+
+//时间转成时间戳
+function getDateTimeStamp(dateStr){
+ return Date.parse(dateStr.replace(/-/gi,"/"));
+}
+//时间转换成str
+function getDateDiff(dateTimeStamp){
+  var minute = 1000 * 60;
+  var hour = minute * 60;
+  var day = hour * 24;
+  var halfamonth = day * 15;
+  var month = day * 30;
+  var now = new Date().getTime();
+  var diffValue = now - dateTimeStamp;
+  if(diffValue < 0){return;}
+  var monthC =diffValue/month;
+  var weekC =diffValue/(7*day);
+  var dayC =diffValue/day;
+  var hourC =diffValue/hour;
+  var minC =diffValue/minute;
+  if(monthC>=1){
+    result="" + parseInt(monthC) + "月前";
+  }
+  else if(weekC>=1){
+    result="" + parseInt(weekC) + "周前";
+  }
+  else if(dayC>=1){
+    result=""+ parseInt(dayC) +"天前";
+  }
+  else if(hourC>=1){
+    result=""+ parseInt(hourC) +"小时前";
+  }
+  else if(minC>=1){
+    result=""+ parseInt(minC) +"分钟前";
+  }else
+  result="刚刚";
+  return result;
+}
+
 //json转form
 function json2Form(json) {  
     var str = [];  
@@ -103,4 +142,7 @@ module.exports = {
   formatShortDate:formatShortDate,
   transHukou:transHukou,
   formatDate2: formatDate2,
+  getDateTimeStamp:getDateTimeStamp,
+  getDateDiff:getDateDiff,
+
 }
